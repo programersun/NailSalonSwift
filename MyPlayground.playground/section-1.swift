9d2f1778c3aee53b91c252017abb42393ef48412
ChinaStart <zxy_start@outlook.com>
2015-04-14 09:40:51 +0800

    Haha

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -3,3 +3,34 @@
 import UIKit
 
 var str = "Hello, playground"
+struct Test
+{
+    var x : Int = 0
+    var y : Int = 0
+    
+    mutating func changeXY(xPosition : Int , yPosition : Int)
+    {
+        x += xPosition
+        y += yPosition
+    }
+}
+
+enum Text : Int
+{
+    case me = 0
+    var x : Int
+    {
+        set
+        {
+            self.x = newValue + 1
+        }
+        get
+        {
+            return self.x
+        }
+    }
+}
+
+var num = Text(rawValue: 0)
+num?.x = 5
+println("\(num!.x)")
